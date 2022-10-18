737c6090fd5df16f4491eac798927b348c175443
NguyenThiQue <quenguyenbt@gmail.com>
2022-10-18 10:25:24 +0700

    add Linh

--- app/src/main/java/com/ntq/appbanhang/CheckConnection.java
+++ app/src/main/java/com/ntq/appbanhang/CheckConnection.java
@@ -9,7 +9,6 @@ public class CheckConnection {
     public static boolean haveNetworkConnection(Context context) {
         boolean haveConnectedWifi = false;
         boolean haveConnectedMobile = false;
-
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo[] netInfo = cm.getAllNetworkInfo();
         for (NetworkInfo ni : netInfo) {
